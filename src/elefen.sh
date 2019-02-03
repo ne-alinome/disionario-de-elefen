46ab12de788dc001fecffeaacf4999e29c217035
Marcos Cruz <github@programandala.net>
2019-02-03 16:31:03 +0100

    Add a wrapper to search de DICT dictionary

---
+++ src/elefen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# elefen
+
+# A dict wrapper to search the Elefen dictionary.
+
+# This file is part of the project
+# Disionario de Elefen
+# (http://ne.alinome.net)
+
+# By Marcos Cruz (programandala.net), 2019
+
+# 2019-01-11
+
+dict -d elefen $@
+
